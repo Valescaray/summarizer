b34820a305b5fd08b64498a571a6a95077f58dbd
Agbasi chukwudum <chukwuduma@yahoo.com>
2025-11-06 21:12:35 +0100

    modiified workflow

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -7,7 +7,36 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# Frontend EC2
+# IAM Role for EC2 to access ECR
+resource "aws_iam_role" "summarizer_ec2_role" {
+  name = "summarizer-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach ECR Read-Only Access to the Role
+resource "aws_iam_role_policy_attachment" "ecr_read_access" {
+  role       = aws_iam_role.summarizer_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Create Instance Profile for EC2
+resource "aws_iam_instance_profile" "summarizer_profile" {
+  name = "summarizer-ec2-profile"
+  role = aws_iam_role.summarizer_ec2_role.name
+}
+
 resource "aws_instance" "frontend" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.frontend_instance_type
@@ -16,12 +45,11 @@ resource "aws_instance" "frontend" {
   key_name               = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
- 
+  iam_instance_profile    = aws_iam_instance_profile.summarizer_profile.name
 
   tags = { Name = "summarizer-frontend" }
 }
 
-# Backend EC2
 resource "aws_instance" "backend" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.backend_instance_type
@@ -30,7 +58,37 @@ resource "aws_instance" "backend" {
   key_name               = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
- 
+  iam_instance_profile    = aws_iam_instance_profile.summarizer_profile.name
 
   tags = { Name = "summarizer-backend" }
 }
+
+
+
+# # Frontend EC2
+# resource "aws_instance" "frontend" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = var.frontend_instance_type
+#   subnet_id              = aws_subnet.public.id
+#   vpc_security_group_ids = [aws_security_group.frontend_sg.id]
+#   key_name               = aws_key_pair.deployer.key_name
+#   associate_public_ip_address = true
+
+ 
+
+#   tags = { Name = "summarizer-frontend" }
+# }
+
+# # Backend EC2
+# resource "aws_instance" "backend" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = var.backend_instance_type
+#   subnet_id              = aws_subnet.public.id
+#   vpc_security_group_ids = [aws_security_group.backend_sg.id]
+#   key_name               = aws_key_pair.deployer.key_name
+#   associate_public_ip_address = true
+
+ 
+
+#   tags = { Name = "summarizer-backend" }
+# }
