11ccf1165eb4dce208e9d5c60495e2564c4c232c
Agbasi chukwudum <chukwuduma@yahoo.com>
2025-11-06 22:27:11 +0100

    modiified workflow

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -7,16 +7,8 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# Reference existing IAM Role
-data "aws_iam_role" "summarizer_ec2_role" {
-  name = "summarizer-ec2-role"
-}
 
-# Reference existing IAM Instance Profile
-data "aws_iam_instance_profile" "summarizer_profile" {
-  name = "summarizer-ec2-profile"
-}
-
+# # Frontend EC2
 resource "aws_instance" "frontend" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.frontend_instance_type
@@ -25,13 +17,12 @@ resource "aws_instance" "frontend" {
   key_name               = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
-  iam_instance_profile    = data.summarizer_profile.name
+ 
 
   tags = { Name = "summarizer-frontend" }
 }
 
-
-# Backend EC2
+# # Backend EC2
 resource "aws_instance" "backend" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.backend_instance_type
@@ -40,35 +31,7 @@ resource "aws_instance" "backend" {
   key_name               = aws_key_pair.deployer.key_name
   associate_public_ip_address = true
 
-  iam_instance_profile    = data.aws_iam_instance_profile.summarizer_profile.name
+ 
 
   tags = { Name = "summarizer-backend" }
 }
-
-# # Frontend EC2
-# resource "aws_instance" "frontend" {
-#   ami                    = data.aws_ami.ubuntu.id
-#   instance_type          = var.frontend_instance_type
-#   subnet_id              = aws_subnet.public.id
-#   vpc_security_group_ids = [aws_security_group.frontend_sg.id]
-#   key_name               = aws_key_pair.deployer.key_name
-#   associate_public_ip_address = true
-
- 
-
-#   tags = { Name = "summarizer-frontend" }
-# }
-
-# # Backend EC2
-# resource "aws_instance" "backend" {
-#   ami                    = data.aws_ami.ubuntu.id
-#   instance_type          = var.backend_instance_type
-#   subnet_id              = aws_subnet.public.id
-#   vpc_security_group_ids = [aws_security_group.backend_sg.id]
-#   key_name               = aws_key_pair.deployer.key_name
-#   associate_public_ip_address = true
-
- 
-
-#   tags = { Name = "summarizer-backend" }
-# }
