8b199abe37b1bf0bdbee60e55a92247120518eb4
Agbasi chukwudum <chukwuduma@yahoo.com>
2025-11-12 10:23:59 +0100

    modified file

--- Summarizer_app1/src/main.jsx
+++ Summarizer_app1/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+console.log("Runtime ENV:", window.env?.VITE_APP_URL);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
